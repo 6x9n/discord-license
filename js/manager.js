3f406c41929ffb629109098332f1b2a64956aa19
unknown <jooosama1976@gmail.com>
2026-08-30 23:49:36 +0300

    fix: point apiBase at canonical Vercel production domain

--- js/manager.js
+++ js/manager.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  apiBase: 'https://discord-license-server.vercel.app',
+  apiBase: 'https://discord-license-server-mythic5.vercel.app',
   trialDuration: 10 * 60 * 1000,
   offlineGraceMs: 24 * 60 * 60 * 1000,
   storage: {
